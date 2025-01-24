0d4a59648d8c5c328eca0ca01d617ea8ff31870f
sim-eun-ji <shimba9865@gmail.com>
2025-01-24 12:39:18 +0900

    build

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import Login from './component/Login';
 
 const App = () => {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path='/' element={
           <div className='app'>
